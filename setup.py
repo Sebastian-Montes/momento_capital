02c3de8d2f8ee7059f2d4cdca2c618f42e952eba
Sergio Sebastián Montes Jiménez <ss.montes.jimenez@gmail.com>
2025-05-05 16:52:23 -0600

    Actualización de la librería - 2025-05-05 16:52:23: scipy debug

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="momento_capital",  # Reemplaza con el nombre de tu librería
-    version="0.2.2",
+    version="0.2.3",
     author="Sergio Montes",
     author_email="ss.montes.jimenez@gmail.com",
     description="MomentoCapital Library",
     packages=find_packages(),
-    install_requires=["matplotlib", "pandas", "numpy"],
+    install_requires=["matplotlib", "pandas", "numpy", "scipy"],
     python_requires=">=3.9",
 )
