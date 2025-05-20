d0bc1663c6120626a9dbbaeb0446a4b2071d760b
volumen-capital <info@volumencapital.com>
2025-05-20 01:24:14 -0600

    final_kamila

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="momento_capital",  # Reemplaza con el nombre de tu librería
-    version="0.3.3",
+    version="0.3.4",
     author="Sergio Montes",
     author_email="ss.montes.jimenez@gmail.com",
     description="MomentoCapital Library",
